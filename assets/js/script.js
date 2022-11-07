ba7ef119862469cd6f7274b0af5e5d0eac33c820
Rodrigo Oliveira <oliveiraroma@gmail.com>
2022-11-06 22:41:03 -0300

    Template String

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,44 @@
-let nome ="Suricato Guy"
-let idade = 21
+//let nome ="Suricato Guy"
+//let idade = 21
 
 //let nomeCompleto = {nome: "Suricato", idade: 90}
 //alert(nome)
-console.log(typeof nome)
-console.log(typeof idade)
+//console.log(typeof nome)
+//console.log(typeof idade)
+/*
+let carro = "Ferrari"
+console.log (carro)
+let preco = "R$ 19,99"
+console.log (preco)
 
+let cidade = "São Paulo"
+console.log (cidade)
+*/
+//Aula 5 --declaração de variaveis e constantes
+/*
+let nome = "SuricatoGuy"
+console.log(nome)
+
+nome = "Rodrigo"
+console.log(nome)
+
+
+var nome = "SuricatoGuy"
+console.log(nome)
+
+nome = "Rodrigo"
+console.log(nome)
+
+const teste = "SuricatoGuy"
+console.log(teste)
+
+teste = "Rodrigo"
+console.log(teste)
+*/
+
+//Aula 5 --declaração de variaveis e constantes
+let nome ="Suricato"
+let sobrenome = "Guy"
+
+let nomeCompleto = `${nome} ${sobrenome}`;
+console.log(nomeCompleto)
