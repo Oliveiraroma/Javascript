228c39638a00096043d02564a4a81d7af2dc9b1b
Rodrigo Oliveira <oliveiraroma@gmail.com>
2022-11-07 00:00:55 -0300

    Aula13-Condicional-IF-ELSE

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,9 @@
-//let nome ="Suricato Guy"
-//let idade = 21
+//Aula13 - Condicionais if - else
 
-//let nomeCompleto = {nome: "Suricato", idade: 90}
-//alert(nome)
-//console.log(typeof nome)
-//console.log(typeof idade)
-/*
-let carro = "Ferrari"
-console.log (carro)
-let preco = "R$ 19,99"
-console.log (preco)
+let idade = 17;
 
-let cidade = "São Paulo"
-console.log (cidade)
-*/
-//Aula 5 --declaração de variaveis e constantes
-/*
-let nome = "SuricatoGuy"
-console.log(nome)
-
-nome = "Rodrigo"
-console.log(nome)
-
-
-var nome = "SuricatoGuy"
-console.log(nome)
-
-nome = "Rodrigo"
-console.log(nome)
-
-const teste = "SuricatoGuy"
-console.log(teste)
-
-teste = "Rodrigo"
-console.log(teste)
-*/
-
-//Aula 5 --declaração de variaveis e constantes
-let nome ="Suricato"
-let sobrenome = "Guy"
-
-let nomeCompleto = `${nome} ${sobrenome}`;
-console.log(nomeCompleto)
+if (idade >= 18){
+    console.log("Você é maior de idade");
+} else {
+    console.log("Você é menor de idade");
+}
