9e27dbdd9f4b45b8d1530f1656abd29a2ad738e6
Rodrigo Oliveira <oliveiraroma@gmail.com>
2022-11-07 22:13:44 -0300

    Retorno de função

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,6 @@
-function somar(n1, n2){
-    let resultado = n1 + n2;
-    console.log('Resultado: ' + resultado)
-}
-somar(10,15);
-
 function nomeCompleto(nome, sobrenome){
-    console.log(`Meu nome completo é ${nome} ${sobrenome}`);//Template String
-    
-    //console.log("Meu nome completo é " nome + ' ' + sobrenome);//String
+    return `${nome} ${sobrenome}`
 }
-nomeCompleto("Rodrigo", "Oliveira");
-nomeCompleto("Matheus","Suricato");
+
+let completo = nomeCompleto("Rodrigo", "Oliveira");
+console.log('Nome Completo: ' + completo);
