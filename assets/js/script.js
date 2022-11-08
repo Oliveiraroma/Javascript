fba23961870afbaa6e23ce4b08f41cd0e4211189
Rodrigo Oliveira <oliveiraroma@gmail.com>
2022-11-07 21:26:33 -0300

    Class 18 - Conditional 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,23 @@
-let idade = "20";
+//Replace the @ with the correct conditionals, the result must be TRUE
+let x = 10;
+let y = 5;
+console.log(x > y);
 
-let mensagem = `Você tem ${idade} anos`;
+//Replace the @ with the correct conditionals, the result must be FALSE
+let w = "10";
+let z = 10;
+console.log(w === z);
 
-if (idade === 20){ //=== verifica junto o tipo da varivel
-    console.log(mensagem)
+//Replace the @ with the correct conditionals, the result must be TRUE
+let a = 10;
+let b = 5;
+console.log(a > b);
+
+//Creat a conditional to check if the meat price is high. Ps: Until 45 is cheap.
+let preco = 40.3
+
+if (preco > 45) {
+    console.log("The meat's price is high")
+} else {
+    console.log("The meat is cheap")
 }
